c2e06b21c321300766b35d33ccdfd2ecf25d33be
Traniel Pride <80042230+tranielmp8@users.noreply.github.com>
2024-05-03 15:09:03 -0500

    Update settings.py

--- audiobookcollection/settings.py
+++ audiobookcollection/settings.py
@@ -18,9 +18,9 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-809b.up.railway.app', '*']
 
-# CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['https://web-production-809b.up.railway.app']
 
 # Application definition
 
